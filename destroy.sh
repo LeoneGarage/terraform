59692a62980478ca432adf222543b7d11aa6478d
Leon Eller <leon.eller@databricks.com>
2022-01-25 11:17:11 +1100

    Destroy account level independent of workspace level

--- destroy.sh
+++ destroy.sh
@@ -33,60 +33,57 @@ DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ACCOUNT_NAME="$(grep databricks_account_name secrets.tfvars | cut -d'=' -f2 | tr -d '"')---account---level"
 
-if [ -z "$WORKSPACE_NAME" ]; then
-W=$(terraform -chdir=$DIR workspace show)
-  if [ "$W" != "default" ] && [ "$W" != "$ACCOUNT_NAME" ]; then
-    WORKSPACE_NAME=$W
-  else
-    if [ "$W" = "default" ]; then
-      echo "Cannot destroy default workpace"
-      echo "run terraform workspace select <workspace name> to change from default or specify -w <workspace name> argument"
-      exit 1
+if [ -z "$ACCOUNT_LEVEL" ] || [ "$ACCOUNT_LEVEL" != "true" ]; then
+  if [ -z "$WORKSPACE_NAME" ]; then
+  W=$(terraform -chdir=$DIR workspace show)
+    if [ "$W" != "default" ] && [ "$W" != "$ACCOUNT_NAME" ]; then
+      WORKSPACE_NAME=$W
     else
-      if [ "$W" = "$ACCOUNT_NAME" ]; then
-        echo "Cannot destroy $ACCOUNT_NAME workpace"
+      if [ "$W" = "default" ]; then
+        echo "Cannot destroy default workpace"
         echo "run terraform workspace select <workspace name> to change from default or specify -w <workspace name> argument"
         exit 1
+      else
+        if [ "$W" = "$ACCOUNT_NAME" ]; then
+          echo "Cannot destroy $ACCOUNT_NAME workpace"
+          echo "run terraform workspace select <workspace name> to change from default or specify -w <workspace name> argument"
+          exit 1
+        fi
       fi
     fi
   fi
-fi
 
-terraform -chdir=$DIR/workspace init
-set +e
-if [ -n "$WORKSPACE_NAME" ]; then
-terraform -chdir=$DIR/workspace workspace select $WORKSPACE_NAME
-fi
-terraform -chdir=$DIR/workspace destroy -auto-approve -var="workspace=$WORKSPACE_NAME"
-DESTROY_EXIT_CODE=$?
-if [ $DESTROY_EXIT_CODE = 0 ]; then
-terraform -chdir=$DIR/workspace workspace select default
-if [ -n "$WORKSPACE_NAME" ]; then
-terraform -chdir=$DIR/workspace workspace delete $WORKSPACE_NAME
-fi
-fi
-set -e
+  terraform -chdir=$DIR/workspace init
+  set +e
+  if [ -n "$WORKSPACE_NAME" ]; then
+  terraform -chdir=$DIR/workspace workspace select $WORKSPACE_NAME
+  fi
+  terraform -chdir=$DIR/workspace destroy -auto-approve -var="workspace=$WORKSPACE_NAME"
+  DESTROY_EXIT_CODE=$?
+  if [ $DESTROY_EXIT_CODE = 0 ]; then
+  terraform -chdir=$DIR/workspace workspace select default
+  if [ -n "$WORKSPACE_NAME" ]; then
+  terraform -chdir=$DIR/workspace workspace delete $WORKSPACE_NAME
+  fi
+  fi
+  set -e
 
-terraform -chdir=$DIR/provision init
-if [ -n "$WORKSPACE_NAME" ]; then
-  terraform -chdir=$DIR/provision workspace select $WORKSPACE_NAME
-fi
-VARFILE="$(cd "$(dirname "secrets.tfvars")"; pwd)/$(basename "secrets.tfvars")"
-terraform -chdir=$DIR/provision destroy -auto-approve -var-file $VARFILE
-terraform -chdir=$DIR/provision workspace select default
-if [ -n "$WORKSPACE_NAME" ]; then
-  terraform -chdir=$DIR/provision workspace delete $WORKSPACE_NAME
-PRIVATELINK_DNS_STATE_FILE=$WORKSPACE_NAME-private-dns.tfvars
-PRIVATELINK_DNS_STATE_FILE="$(cd "$(dirname "$PRIVATELINK_DNS_STATE_FILE")"; pwd)/$(basename "$PRIVATELINK_DNS_STATE_FILE")"
-rm -f "$PRIVATELINK_DNS_STATE_FILE"
-fi
-
-if [ -n "$ACCOUNT_LEVEL" ] && [ "$ACCOUNT_LEVEL" = "true" ]; then
-  terraform -chdir=$DIR/provision/log-delivery init
-  terraform -chdir=$DIR/provision/log-delivery workspace select $ACCOUNT_NAME
-  terraform -chdir=$DIR/provision/log-delivery destroy -auto-approve -var-file $VARFILE
-  terraform -chdir=$DIR/provision/log-delivery workspace select default
-  terraform -chdir=$DIR/provision/log-delivery workspace delete $ACCOUNT_NAME
+  terraform -chdir=$DIR/provision init
+  if [ -n "$WORKSPACE_NAME" ]; then
+    terraform -chdir=$DIR/provision workspace select $WORKSPACE_NAME
+  fi
+  VARFILE="$(cd "$(dirname "secrets.tfvars")"; pwd)/$(basename "secrets.tfvars")"
+  terraform -chdir=$DIR/provision destroy -auto-approve -var-file $VARFILE
+  terraform -chdir=$DIR/provision workspace select default
+  if [ -n "$WORKSPACE_NAME" ]; then
+    terraform -chdir=$DIR/provision workspace delete $WORKSPACE_NAME
+  fi
+  else
+    terraform -chdir=$DIR/provision/log-delivery init
+    terraform -chdir=$DIR/provision/log-delivery workspace select $ACCOUNT_NAME
+    terraform -chdir=$DIR/provision/log-delivery destroy -auto-approve -var-file $VARFILE
+    terraform -chdir=$DIR/provision/log-delivery workspace select default
+    terraform -chdir=$DIR/provision/log-delivery workspace delete $ACCOUNT_NAME
 fi
 
 
