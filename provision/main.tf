07da995faeec23a45c57aacb7911b862ae5a498f
Leon Eller <leon.eller@databricks.com>
2022-03-31 14:51:14 +1100

    Upgraded terraform provider

--- provision/main.tf
+++ provision/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     databricks = {
       source  = "databrickslabs/databricks"
-      version = "0.4.5"
+      version = "0.5.4"
     }
     aws = {
       source  = "hashicorp/aws"
